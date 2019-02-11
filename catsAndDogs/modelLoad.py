eb30db942bba8152a92025087a5ccecc04b42f9e
joe hage <joehage96@gmail.com>
2019-02-11 14:51:44 +0200

    Add files via upload

--- catsAndDogs/modelLoad.py
+++ catsAndDogs/modelLoad.py
@@ -27,19 +27,20 @@ def checkSpecie(path):
     else:
         print('cat\n')
        
-# To check the class of all the pics inside a directory
-directory = 'dataset/test_set/cats'
-
-for filename in os.listdir():
-    test_image=image.load_img(path='dataset/test_set/cats/'+filename, target_size=(128, 128))
-    test_image=image.img_to_array(test_image)
-    test_image=np.expand_dims(test_image, axis=0)   
-    result=classifier.predict(test_image)  
-    animal+=1
-    if result[0][0]==1:
-        dog+=1
-    else:
-        cat+=1
+for i in range(4):
+    directory = ['dataset/test_set/cats','dataset/training_set/cats','dataset/test_set/dogs','dataset/training_set/dogs']
+    
+    print('testing '+directory[i]+'...')
+    for filename in os.listdir(directory[i]):
+        test_image=image.load_img(path=directory[i]+'/'+filename, target_size=(128, 128))
+        test_image=image.img_to_array(test_image)
+        test_image=np.expand_dims(test_image, axis=0)   
+        result=classifier.predict(test_image)  
+        animal+=1
+        if result[0][0]==1:
+            dog+=1
+        else:
+            cat+=1
 
 print('animal count is : '+str(animal)+'\ncat count is: '+str(cat)+'\ndog count is: '+str(dog))
 
