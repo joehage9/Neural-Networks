d0ed56e8dcf8f3db0aa9d52a4494c22237864b80
joe hage <joehage96@gmail.com>
2019-02-12 01:18:08 +0200

    Add files via upload

--- catsAndDogs/modelLoad.py
+++ catsAndDogs/modelLoad.py
@@ -4,11 +4,12 @@ Created on Fri Feb  8 13:48:32 2019
 
 @author: joe hage
 """
+
 from keras.models import load_model
 from keras.preprocessing import image
 import os
 import numpy as np
-
+import time 
 classifier=load_model("dogAndCatCNNModel.h5")
 
 animalNb=0
@@ -17,6 +18,39 @@ catNb=0
         
 directory = ['dataset/test_set/cats','dataset/training_set/cats','dataset/test_set/dogs','dataset/training_set/dogs']  
 #directory=['dataset/single_prediction/cats']
+
+# To check the class of one pic
+def checkSpecie(path):   
+    test_image=image.load_img(path=path, target_size=(128, 128))
+    test_image=image.img_to_array(test_image)
+    test_image=np.expand_dims(test_image, axis=0)   
+    result=classifier.predict(test_image)  
+    if result[0][0]==1:
+        print('dog\n')
+    else:
+        print('cat\n') 
+        
+def stopWatch(value):
+    valueD = (((value/365)/24)/60)
+    Days = int (valueD)    
+    valueH = (valueD-Days)*365
+    Hours = int(valueH)    
+    valueM = (valueH - Hours)*24
+    Minutes = int(valueM)    
+    valueS = (valueM - Minutes)*60
+    Seconds = int(valueS)  
+    if Days>1:
+        print('Time elapsed: '+str(Days)+' days '+str(Hours)+' hours '+str(Minutes)+'minutes '+str(Seconds)+' seconds')
+    elif Hours>1:
+        print('Time elapsed: '+str(Hours)+' hours '+str(Minutes)+'minutes '+str(Seconds)+' seconds')
+    elif Minutes>1:
+        print('Time elapsed: '+str(Minutes)+'minutes '+str(Seconds)+' seconds')
+    elif Seconds>1:
+        print('Time elapsed: '+str(Seconds)+' seconds')
+        
+# Stopwatch starts
+start = time.time() 
+
 for i in range(len(directory)):
     print('testing '+directory[i]+'...')
     for filename in os.listdir(directory[i]):
@@ -31,16 +65,9 @@ for i in range(len(directory)):
             dogNb+=1
         else:           
             catNb+=1
-
 print('animal count is : '+str(animalNb)+'\ncat count is: '+str(catNb)+'\ndog count is: '+str(dogNb))
 
-# To check the class of one pic
-def checkSpecie(path):   
-    test_image=image.load_img(path=path, target_size=(128, 128))
-    test_image=image.img_to_array(test_image)
-    test_image=np.expand_dims(test_image, axis=0)   
-    result=classifier.predict(test_image)  
-    if result[0][0]==1:
-        print('dog\n')
-    else:
-        print('cat\n')      
+# Stopwatch ends
+end = time.time()   
+# Check time elapsed   
+stopWatch(end-start)
